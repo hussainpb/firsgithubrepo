188833054c231ada07bc2be8870a517cd7f27932
hussainpb <128016230+hussainpb@users.noreply.github.com>
2023-04-16 13:49:21 -0400

    commit

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Hussain Ahmed
+ *
+ */
+module first_selenium {
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.chrome_driver;
+}
---
+++ src/variousConcept/LaunchChrome.java
@@ -0,0 +1,79 @@
+package variousConcept;
+
+import org.openqa.selenium.WebDriver;
+
+public class LaunchChrome {
+	
+static WebDriver driver;
+
+
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// properties//variable//fields
+		// methods//contains statements
+		
+		LaunchBrowser();
+		positiveLoginTest();
+		tearDown();
+		
+		LaunchBrowser();
+		negLoginTest();
+		tearDown();
+		
+		LaunchBrowser();
+		negLoginTest1();
+		tearDown();
+		
+
+		LaunchBrowser();
+		negLoginTest2();
+		tearDown();
+		
+
+	}
+
+
+
+	private static void negLoginTest2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	private static void negLoginTest1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	private static void negLoginTest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	private static void tearDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	private static void positiveLoginTest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	private static void LaunchBrowser() {
+					
+				
+		
+	}
+
+}
